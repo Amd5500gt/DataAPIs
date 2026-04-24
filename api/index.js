1686dc1159e5113f7937dd68358ffccc3a106bc2
amd5500gt <jasss.pb03@gmail.com>
2026-04-25 00:17:51 +0530

    addd landing page

--- api/index.js
+++ api/index.js
@@ -3,7 +3,20 @@ const fs = require("fs")
 const path = require("path")
 
 const app  = express()
-app.use("/",(req,res)=>{
+const landingPage = express.static(path.join(process.cwd(),"public/index.html"))
+
+app.get("/",(req,res)=>{
+    try{
+ 
+        res.json(landingPage)   // ✅ better than res.send
+        console.log("success")
+    }
+    catch (err){
+        console.log(err)
+        res.send("something went wrong, contact owner ‼️🥷\n🅾 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 : harjeet_vx")
+    }
+})
+app.get("/data",(req,res)=>{
     try{
            const filePath = path.join(process.cwd(), "data.json")
         const data = fs.readFileSync(filePath, "utf-8")
