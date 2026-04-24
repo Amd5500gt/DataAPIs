44c9429bf64d935209349ca03fba8e180f99e36f
amd5500gt <jasss.pb03@gmail.com>
2026-04-25 00:04:27 +0530

    update vercel config

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
 const express = require("express")
 const fs = require("fs")
+const path = require("path")
+
 const app  = express()
 app.use("/",(req,res)=>{
     try{
-        const data = fs.readFileSync("./data.json","utf-8")
-        res.send(JSON.parse(data))
+           const filePath = path.join(process.cwd(), "data.json")
+        const data = fs.readFileSync(filePath, "utf-8")
+
+        res.json(JSON.parse(data))   // ✅ better than res.send
         console.log("success")
     }
     catch (err){
         console.log(err)
-        res.send("something went wrong, contact owner ‼️🥷")
+        res.send("something went wrong, contact owner ‼️🥷\n🅾 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 : harjeet_vx")
     }
 })
 
