127f1ff96d301bab1408189a4ea9a1684a10b053
amd5500gt <jasss.pb03@gmail.com>
2026-04-24 23:59:34 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const fs = require("fs")
 const app  = express()
-const data = fs.readFileSync("./data.json","utf-8")
 app.use("/",(req,res)=>{
     try{
+        const data = fs.readFileSync("./data.json","utf-8")
         res.send(JSON.parse(data))
         console.log("success")
     }
